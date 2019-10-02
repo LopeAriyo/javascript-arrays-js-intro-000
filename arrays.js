65940edc445de9f080406aef5031abd6e089b145
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-02 16:20:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,5 @@
 var  chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var addElementToBeginningOfArray = function(){}
+var destructivelyAddElementToBeginningOfArray = function(){}
+var addElementToEndOfArray = function (){}
+var destructivelyAddElementToEndOfArray = function(){} 
