6b39040a5b453dce7070b51f5d1d208002c492cf
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-02 16:25:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var  chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
 function addElementToBeginningOfArray (){}
-var destructivelyAddElementToBeginningOfArray = function(,){}
-var addElementToEndOfArray = function (,){}
-var destructivelyAddElementToEndOfArray = function(,){}
+//var destructivelyAddElementToBeginningOfArray = function(,){}
+//var addElementToEndOfArray = function (,){}
+//var destructivelyAddElementToEndOfArray = function(,){}
