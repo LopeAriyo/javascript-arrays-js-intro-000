3b5ab1471e06e00edfb6c1cc41ad1a177f2f5ae6
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-02 16:53:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,18 +1,23 @@
 var  chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
-var addElementToBeginningOfArray = function(array1, element1){}
-var destructivelyAddElementToBeginningOfArray = function(array2, element2){}
+var addElementToBeginningOfArray = function(array, element){
+
+  NewArray = ["foo", ...array]
+  return NewArray
+
+  //do not alter original array
+}
+var destructivelyAddElementToBeginningOfArray = function(array, element){
+  array.unshift('foo')
+  return array
+
+}
+
+  
 
 
-/* Both functions take two parameters,
-an array and an element to add to the beginning of the array, and both
-functions should add the element to the beginning of the array and then return
-the whole array.
 
-The destructive function, destructivelyAddElementToBeginningOfArray, should
-alter the original array that's passed in; addElementToBeginningOfArray, on the
-other hand, should return a new array and not modify the original.*/
 
 //funtion addElementToEndOfArray (){}
 //function destructivelyAddElementToEndOfArray (){}
