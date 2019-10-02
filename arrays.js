980ddb0a827d588bb402caedff82af72bc21742e
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-02 17:13:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,19 @@ var accessElementInArray = function (array, index){
 return array[index];
 
  }
+
+ var destructivelyRemoveElementFromBeginningOfArray = function (array){
+   array.shift
+   return array
+ }
+
+ 
+ var destructivelyRemoveElementFromEndOfArray = function (array){}
+
+ /*
+Define a function in arrays.js called removeElementFromBeginningOfArray. It 
+takes an array as its only argument. The function should remove the first 
+element in the array. This function should return the entire array in the same 
+line, and it should not mutate the original array.
+ */
+ 
